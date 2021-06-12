158d118b40f3ca3acd9999994bc521f44ef2594a
Brendan Choy <maledictionemaeternitatis@gmail.com>
2021-06-12 23:39:53 +1000

    Added Ju7mp + CameraCollision

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,34 +8,78 @@ public class PlayerMovement : MonoBehaviour
     public Transform playerCameraParent;
     public float lookSpeed = 2.0f;
     public float lookXLimit = 60.0f;
-    public float speed = 7.5f;
+    public float speed = 0.1f;
+    public float jumpSpeed = 1.0f;
+    public float maxspeed = 1.5f;
     Vector3 moveDirection = Vector3.zero;
     private Rigidbody rb;
+    public Vector3 Ground_check_position;
+    public bool Jetpack = false;
+
+
+    [HideInInspector]
+    public bool canMove = true;
+    public bool grounded = true;
 
 
     // Start is called before the first frame update
     void Start()
     {
-           rb = GetComponent<Rigidbody>();
+          rb = GetComponent<Rigidbody>();
+          rotation.y = transform.eulerAngles.y;
     }
 
     // Update is called once per frame
     void Update()
     {
+        grounded = false;
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 right = transform.TransformDirection(Vector3.right);
 
-        float curSpeedX = speed * Input.GetAxis("Vertical");
-        float curSpeedY = speed * Input.GetAxis("Horizontal");
+        float curSpeedX = canMove ? speed * Input.GetAxis("Vertical"):0;
+        float curSpeedY = canMove ? speed * Input.GetAxis("Horizontal"):0;
         moveDirection = (forward * curSpeedX) + (right * curSpeedY);
-        if (Input.GetButton("Jump"))
+        
+        moveDirection = Vector3.ClampMagnitude(moveDirection, maxspeed);
+
+
+        Collider[] colliders = Physics.OverlapBox(transform.position+Ground_check_position, Vector3.one / 2);
+        for (int i = 0; i < colliders.Length; i++)
         {
-            moveDirection.y = speed;
+            if (colliders[i].gameObject != gameObject)
+            {
+
+                grounded = true;
+
+                if (!grounded)
+                    //first landing
+                    i = colliders.Length;
+            }
         }
-        if (Input.GetButton("Fire3"))
+
+        if (grounded)
         {
-            moveDirection.y = -speed;
+            if (Input.GetButton("Jump"))
+            {
+                rb.velocity = new Vector3(rb.velocity.x,jumpSpeed, rb.velocity.z);
+            }
         }
+        else if (Jetpack)
+        {
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = speed;
+            }
+
+            if (Input.GetButton("Fire3"))
+            {
+                moveDirection.y = -speed;
+            }
+        }
+        
+        
+
+
         rb.AddForce(moveDirection, ForceMode.Impulse);
 
 
