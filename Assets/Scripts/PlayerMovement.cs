de60d5aa1653ab0fd65c520238f6dd6e329cdffb
Brendan Choy <maledictionemaeternitatis@gmail.com>
2021-06-12 20:37:30 +1000

    Player Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Vector2 rotation = Vector2.zero;
+    public Transform playerCameraParent;
+    public float lookSpeed = 2.0f;
+    public float lookXLimit = 60.0f;
+    public float speed = 7.5f;
+    Vector3 moveDirection = Vector3.zero;
+    private Rigidbody rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+           rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+
+        float curSpeedX = speed * Input.GetAxis("Vertical");
+        float curSpeedY = speed * Input.GetAxis("Horizontal");
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+        if (Input.GetButton("Jump"))
+        {
+            moveDirection.y = speed;
+        }
+        if (Input.GetButton("Fire3"))
+        {
+            moveDirection.y = -speed;
+        }
+        rb.AddForce(moveDirection, ForceMode.Impulse);
+
+
+
+
+        rotation.y += Input.GetAxis("Mouse X") * lookSpeed;
+        rotation.x += -Input.GetAxis("Mouse Y") * lookSpeed;
+        rotation.x = Mathf.Clamp(rotation.x, -lookXLimit, lookXLimit);
+        playerCameraParent.localRotation = Quaternion.Euler(rotation.x, 0, 0);
+        transform.eulerAngles = new Vector2(0, rotation.y);
+    }
+}
