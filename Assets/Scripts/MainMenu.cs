11d8ce65c41efcc69c5701f4fa3abf517e20efbf
PhantomNeon <guptaishaan06@gmail.com>
2021-06-12 15:31:48 +0530

    Test Commit
    Added some scripts

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Play()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/OxygenMeter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class OxygenMeter : MonoBehaviour
+{
+    public float OxygenDepletionMultiplier;
+    public float Oxygen = 100f;
+    public float MaxOxygenLevel = 100;
+
+    private void Awake()
+    {
+        Oxygen = MaxOxygenLevel;
+    }
+
+    private void Update()
+    {
+        Oxygen -= Time.deltaTime * OxygenDepletionMultiplier;
+    }
+
+    public void ReplenishOxygen()
+    {
+        Oxygen = MaxOxygenLevel;
+    }
+}
